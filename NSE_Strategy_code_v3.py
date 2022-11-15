c4ced4621f7b724e6e6d08ef9ea92607371bc787
Pritish Nayak <40473746+Pritish52718@users.noreply.github.com>
2022-11-15 13:02:45 +0530

    Update NSE_Strategy_code_v3.py

--- NSE_Strategy_code_v3.py
+++ NSE_Strategy_code_v3.py
@@ -250,10 +250,7 @@ with st.sidebar.header('Choose your input type'):
 st.sidebar.write('Your selected input type:', check_type)
 
 
-###changes start
-# if st.button('Download latest Data'):
-#         st.experimental_memo.clear()
-#         st. experimental_rerun()
+
 
 if check_type=='NSE_stocks':
     col1,col2,col3,col4,col5=st.columns([2,1.5,1.5,1.5,1.5])
@@ -296,8 +293,6 @@ if check_type=='NSE_stocks':
 elif check_type=='NSE_filter':
     #st.session_state.co=co
     col1,col2,col3,col4=st.columns([2,2,2,2])
-#     if st.button('Reset',1):
-#         st.experimental_memo.clear()
 
     INSTRUMENT=col1.radio('Select Stock option or Index option',("OPTSTK","OPTIDX"))
 
